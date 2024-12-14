ccefeeeffd70407f508b5122b6f3ec208593f19c
naojulius <naoj8lius@gmail.com>
2024-12-14 23:45:10 +0300

    added bottom navigation bar

--- components/widget/navigation/bottom.vue
+++ components/widget/navigation/bottom.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
 </script>
 <template>
-  BOTTOMMM BEEE
+  <div class="w-full fixed bottom-0 p-[3.5px]">
+     <div class="rounded-xl bg-gray-300 border border-black size-full flex flex-row justify-center items-center p-1 gap-1">
+        <Button class=" w-1/4 h-full bg-gray-100 flex flex-col items-center justify-center">
+          <img src="/icons/harvester.png" alt="harvester" class="w-10 h-10">
+          <span class="text-gray-700 font-bold w-full h-full -mt-2">Harvest</span>
+        </Button>
+        <Button class=" w-1/4 h-full bg-gray-100 flex flex-col items-center justify-center">
+          <img src="/icons/fertilizer.png" alt="harvester" class="w-10 h-10">
+          <span class="text-gray-700 font-bold w-full h-full -mt-2">Harvest</span>
+        </Button>
+        <Button class=" w-1/4 h-full bg-gray-100 flex flex-col items-center justify-center">
+          <img src="/icons/money-bag.png" alt="harvester" class="w-10 h-10">
+          <span class="text-gray-700 font-bold w-full h-full -mt-2">Harvest</span>
+        </Button>
+        <Button class=" w-1/4 h-full bg-gray-100 flex flex-col items-center justify-center">
+          <img src="/icons/fortune-wheel.png" alt="harvester" class="w-10 h-10">
+          <span class="text-gray-700 font-bold w-full h-full -mt-2">Harvest</span>
+        </Button>
+     </div>
+  </div>
 </template>
