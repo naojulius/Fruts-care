8902a9d7d431e58bcd1aab99fdb61303b93a3230
naojulius <naoj8lius@gmail.com>
2024-12-14 23:13:06 +0300

    configure

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+      <NuxtPage />
+      <WidgetNavigationBottom />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'], 
+  app: {
+    head: {
+      meta: [
+        {charset: "utf-8"},
+        {name: "viewport", content: "width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"},
+        {"http-equiv" : "X-UA-Compatible", content:"ie=edge"},
+      ]
+    }
+  },
   shadcn: {
     /**
      * Prefix for all the imported component
